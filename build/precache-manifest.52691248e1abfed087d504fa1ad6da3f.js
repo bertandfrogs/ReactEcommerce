5f2736ad4a81cd629fa8ff82b666a6b9939f64b8
Sarah Smalley <bertandfrogs@gmail.com>
2019-05-01 08:33:45 -0600

    firebase hosting

---
+++ build/precache-manifest.52691248e1abfed087d504fa1ad6da3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "03db1f5dd9a540169397",
+    "url": "/static/js/main.28a7aaad.chunk.js"
+  },
+  {
+    "revision": "d750354ac3bc022d3db7",
+    "url": "/static/js/2.84d84479.chunk.js"
+  },
+  {
+    "revision": "03db1f5dd9a540169397",
+    "url": "/static/css/main.4887a8fb.chunk.css"
+  },
+  {
+    "revision": "db7a284e3cd5c8eac18c3b98ebe134a9",
+    "url": "/index.html"
+  }
+];
